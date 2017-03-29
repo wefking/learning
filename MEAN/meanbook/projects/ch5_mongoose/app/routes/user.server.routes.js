80a5274a2b5751c16353d476a3d2d941d87908c9
William Fonseca <william.fonseca@gmail.com>
2017-03-29 06:20:03 +0200

    Reading a single user document using findOne()

--- MEAN/meanbook/projects/ch5_mongoose/app/routes/user.server.routes.js
+++ MEAN/meanbook/projects/ch5_mongoose/app/routes/user.server.routes.js
@@ -2,4 +2,6 @@ const users = require('../../app/controllers/users.server.controller');
 
 module.exports = function(app) {
   app.route('/users').post(users.create).get(users.list);
+  app.route('/users/:userId').get(users.read);
+  app.param('userId', users.userByID);
 };
