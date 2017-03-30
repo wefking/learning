2f3a654ca91b2642ab4baeec946a7eba80b2dee0
William Fonseca <william.fonseca@gmail.com>
2017-03-30 05:49:22 +0200

    Updating an existing document

--- MEAN/meanbook/projects/ch5_mongoose/app/controllers/users.server.controller.js
+++ MEAN/meanbook/projects/ch5_mongoose/app/controllers/users.server.controller.js
@@ -37,3 +37,16 @@ exports.userByID = function(req, res, next, id) {
   }
   });
 };
+
+exports.update = function(req, res, next) {
+  User.findByIdAndUpdate(req.user.id, req.body, {
+    'new': true
+    }, (err, user) => {
+    if (err) {
+      return next(err);
+    } else {
+      res.status(200).json(user);
+    }
+  });
+};
+
--- MEAN/meanbook/projects/ch5_mongoose/app/routes/user.server.routes.js
+++ MEAN/meanbook/projects/ch5_mongoose/app/routes/user.server.routes.js
@@ -2,6 +2,6 @@ const users = require('../../app/controllers/users.server.controller');
 
 module.exports = function(app) {
   app.route('/users').post(users.create).get(users.list);
-  app.route('/users/:userId').get(users.read);
+  app.route('/users/:userId').get(users.read).put(users.update);
   app.param('userId', users.userByID);
 };
