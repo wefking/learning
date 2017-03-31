d492d7b7193486efc6a34db420071261c375fad7
William Fonseca <william.fonseca@gmail.com>
2017-03-31 05:58:17 +0200

    Adding Mongoose custom setter modifiers

--- MEAN/meanbook/projects/ch5_mongoose/app/models/user.server.model.js
+++ MEAN/meanbook/projects/ch5_mongoose/app/models/user.server.model.js
@@ -13,6 +13,19 @@ const UserSchema = new Schema({
   created: {
     type: Date,
     default: Date.now
+  },
+  website: {
+    type: String,
+    set: function(url) {
+      if (!url) {
+        return url;
+      } else {
+        if (url.indexOf('http://') !==0 && url.indexOf('https://') !== 0) {
+          url = 'http://' + url;
+        }
+      return url;
+      }
+    }
   }
 });
 
