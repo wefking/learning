819b1b9ae6d16f1bdf943004bf722957d9fbe569
William Fonseca <william.fonseca@gmail.com>
2017-04-07 20:47:33 +0200

    Modify user controller to connect model and views.
    New methods for handling signup, signout and rendering.
    Error lookup and messages also included.

--- MEAN/meanbook/projects/ch6_passport/app/controllers/users.server.controller.js
+++ MEAN/meanbook/projects/ch6_passport/app/controllers/users.server.controller.js
@@ -1,4 +1,105 @@
 const User = require('mongoose').model('User');
+const passport = require('passport');
+
+
+/**
+ * Private method that returns a unified error message from a Mongoose
+ * error object.
+ * There are two possible errors here:
+ * - MongoDB indexing error handled using error code
+ * - Mongoose calidation error handled using the err.errors
+ * @param {Mongoose object} err 
+ * @return {String message
+ */
+function getErrorMessage(err) {
+  let message = '';
+
+  if (err.code) {
+    switch (err.code) {
+      case 11000;
+      case 11001;
+        message = 'Username already existis';
+        break;
+      default:
+        message = 'Something went wrong';
+    }
+  } else {
+    for (var errName in err.errors) {
+      if (err.errors[errName].message) message = err.errors[errName].message;
+    }
+  }
+  return message;
+};
+
+/**
+ * Render sign-in page
+ */
+exports.renderSignin = function(req, res, next) {
+  if (!req.user) {
+    res.render('signin', {
+      title: 'Sign-in Form',
+      messages: req.flash('error') || req.flash('info')
+    });
+  } else {
+    return res.redirect('/');
+  }
+};
+
+/**
+ * Render sign-up page
+ */
+exports.renderSignup = function(req, res, next) {
+  if (!req.user) {
+    res.render('signup', {
+      title: 'Sign-up Form',
+      messages: req.flash('error')
+    });
+  } else {
+    return res.redirect('/');
+  }
+};
+
+/**
+ * Uses the User model to create new users
+ * Creates a user object from the HTTP request body
+ * Try saving the user object to MongoDB
+ * If error occurs then the appropiate message will be provided to user
+ * If user creation was succesful, the user session will be created
+ * using the req.login() method. (establish a succesful login session)
+ * Provided by Passport
+ * After the login operation is completed, a user object will be signed
+ * to the req.user object
+ */
+exports.signup = function(req, res, next) {
+  if (!req.user) {
+    const user = new User(req.body);
+    user.provider = 'local';
+
+    user.save((err) => {
+      if (err) {
+        const message = getErrorMessage(err);
+
+        req.flash('error', message);
+        return res.redirect('/signup');
+      }
+      req.login(user, (err) => {
+        if (err) return next(err);
+        return res.redirect('/');
+      });
+    });
+  } else {
+    return res.redirect('/');
+  }
+};
+
+/**
+ * Invalidate the authentication session
+ * It uses the req.logout() method provided by Passport.
+ */
+exports.signout = function(req, res) {
+  req.logout();
+  res.redirect('/');
+};
 
 exports.create = function(req, res, next) {
   const user = new User(req.body);
