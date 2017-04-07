6f7dc2941b3a64a12bde4b0cb3e0cdcda1f9e7c1
William Fonseca <william.fonseca@gmail.com>
2017-04-07 04:15:22 +0200

    Install and configure passport's local strategy
    Passport local strategy is a Node.js module thata allows you to
    implement a username/password mechanism.
    It needs to be installed and oonfigured to use the User Mongoose
    Model.
    Each authentication strategy should be configured in its own
    separated file.

--- MEAN/meanbook/projects/ch6_passport/app/models/user.server.model.js
+++ MEAN/meanbook/projects/ch6_passport/app/models/user.server.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const crypto = require('crypto');
 
 const UserSchema = new Schema({
   firstName: String,
@@ -7,23 +8,31 @@ const UserSchema = new Schema({
   email: {
     type: String,
     index: true,
-    match: /.+\@.+\..+/
+    match: [/.+\@.+\..+/, "Please fill a valid e-mail address"]
   },
   username: {
     type: String,
     trim: true,
-    unique: true,
+    unique: 'Username is required',
     required: true
   },
   password: {
     type: String,
-    validate: [
-      function(password) {
-        return password.length >= 6;
+    validate: [(password) => {
+        return password && password.length > 6;
       },
       'Password should be longer'
     ]
   },
+  salt: {
+    type: String,
+  },
+  provider: {
+    type: String,
+    required: 'Provider is required'
+  },
+  providerId: String,
+  providerData: {},
   created: {
     type: Date,
     default: Date.now
@@ -46,6 +55,7 @@ const UserSchema = new Schema({
     enum: ['Admin', 'Owner', 'User']
   }
 });
+
 UserSchema.virtual('fullName').get(function() {
   return this.firstName + ' ' + this.lastName;
   }).set(function(fullName) {
@@ -53,6 +63,40 @@ UserSchema.virtual('fullName').get(function() {
     this.firstName = splitName[0] || ' ';
     this.lastName = splitName[1] || ' ';
 });
+
+UserSchema.pre('save', function(next) {
+  if (this.password) {
+    this.salt = new Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+    this.password = this.hashPassword(this.password);
+  }
+  next();
+});
+
+UserSchema.methods.hashPassword = function(password) {
+  return crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString('base64');
+};
+
+UserSchema.methods.authenticate = function(password) {
+  return this.password === this.hashPassword(password);
+};
+
+UserSchema.statics.findUniqueUsername = function(username, suffix, callback) {
+  var possibleUsername = username + (suffix || '');
+  this.findOne({
+    username: possibleUsername
+  }, (err, user) => {
+    if (!err) {
+      if (!user) {
+        callback(possibleUsername);
+      } else {
+        return this.findUniqueUsername(username, (suffix || 0) + 1, callback);
+      }
+    } else {
+      callback(null);
+    }
+  });
+};
+
 UserSchema.post('save', function(next) {
   console.log('The user "'+this.username+'" details were saved.');
 });
