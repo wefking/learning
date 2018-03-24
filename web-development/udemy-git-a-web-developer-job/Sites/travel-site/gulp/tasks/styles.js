70364fc9e2c93c264be91093bd174c90958a255d
William Fonseca <william.fonseca@gmail.com>
2018-03-24 23:44:50 +0100

    add a media query for big screens

--- web-development/udemy-git-a-web-developer-job/Sites/travel-site/gulp/tasks/styles.js
+++ web-development/udemy-git-a-web-developer-job/Sites/travel-site/gulp/tasks/styles.js
@@ -3,7 +3,8 @@ postcss =  require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
    return gulp.src('./app/assets/styles/styles.css')
